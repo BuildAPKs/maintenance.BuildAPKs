65a7665a70c00e3410ae4b275d65cdf344e8efcd
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-11-16 03:21:14 -0500

    modified:   reset.repository.sh 	modified:   sha512.sum

--- reset.repository.sh
+++ reset.repository.sh
@@ -1,14 +1,18 @@
 #!/bin/env sh 
 # Copyright 2017-2019 by SDRausty. All rights reserved.
-# Website for this project at https://sdrausty.github.io/TermuxArch
-# See https://sdrausty.github.io/TermuxArch/CONTRIBUTORS Thank You 
-# Use with caution!  Backing up the repository is highly recommended!
+# Use with caution!  Backing up the repository is recommended.
 #############################################################################
 set -eu 
+LPDIR="${PWD%/*}" # http://tldp.org/LDP/abs/html/parameter-substitution.html#PATTMATCHING
+PDIR="${LPDIR##*/}"
+WDIR="${PWD##*/}" # http://tldp.org/LDP/abs/html/refcards.html#AEN22728
 mv .git/config ~/saved_git_config
 rm -rf .git
 git init
 git add .
 git commit 
 cp ~/saved_git_config .git/config
-git push https://github.com/BuildAPKs/"${PWD##*/}" --force master
+git push "https://github.com/$PDIR/$WDIR" --force master
+
+
+# reset.repository.sh EOF
