72d7ab6fc3f7451712210cf04643fd256ffaaa6f
SDRausty <27742457+sdrausty@users.noreply.github.com>
2019-12-13 18:39:13 -0500

    commit 230110371 in 2019

---
+++ sn.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash 
+# Copyright 2017-2019 by SDRausty. All rights reserved.
+# SDRausty https://sdrausty.github.io 
+# Used for creating the commit message in conjunction with `gr.sh`.
+#####################################################################
+set -e 
+ntime=`date +%N`
+if [[ "$ntime" -eq N ]]
+then
+ntime=$(echo $RANDOM)
+until [[ ${#ntime} -gt 8 ]]
+do
+	ntime=$ntime$RANDOM
+done
+fi
+ndate=`date +%Y%m%d`
+printf "%s\\n" "commit $ntime on $ndate"
+# sn.sh EOF
