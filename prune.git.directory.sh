fb2a47fad5af07546326de571d5c5b0df055ad5d
SDRausty <27742457+SDRausty@users.noreply.github.com>
2022-01-21 15:43:56 -0500

    new file:   prune.git.directory.sh modified:   reset.repository.sh

--- prune.git.directory.sh
+++ prune.git.directory.sh
@@ -1,16 +1,11 @@
 #!/bin/env sh 
-# Copyright 2017-2022 by SDRausty. All rights reserved.
+# Copyright 2022 by SDRausty. All rights reserved.
 # Use with caution!  Backing up the repository is recommended.
 #############################################################################
 set -eu 
 LPDIR="${PWD%/*}" # http://tldp.org/LDP/abs/html/parameter-substitution.html#PATTMATCHING
 PDIR="${LPDIR##*/}"
 WDIR="${PWD##*/}" # http://tldp.org/LDP/abs/html/refcards.html#AEN22728
-mv .git/config ~/saved_git_config
-rm -rf .git
-git init
-git add .
-git commit -m "reset $(sn.sh)" # copy sn.sh to path before using this script 
-cp ~/saved_git_config .git/config
+git gc --aggressive --prune
 git push "https://github.com/$PDIR/$WDIR" --force main
 # reset.repository.sh EOF
